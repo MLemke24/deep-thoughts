cc89388df1172ca94f5977b2036f75240b5c3ef4
mlemke24 <mattrlemke@gmail.com>
2021-06-28 14:19:03 -0500

    mod 1

--- server/models/Reaction.js
+++ server/models/Reaction.js
@@ -1,29 +1,28 @@
 const { Schema } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-
 const reactionSchema = new Schema(
-{
+  {
     reactionBody: {
-        type: String,
-        required: true,
-        maxlength: 280
+      type: String,
+      required: true,
+      maxlength: 280
     },
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     createdAt: {
-        type: Date,
-        default: Date.now,
-        get: timestamp => dateFormat(timestamp)
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
     }
-},
-{
+  },
+  {
     toJSON: {
-        getters: true
+      getters: true
     }
-}
+  }
 );
 
-module.exports = reactionSchema
+module.exports = reactionSchema;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+
+const{ typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// create a new Apollo server and pass in schemas
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+// integrate Apollo server with Express app as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -10,6 +22,11 @@ app.use(express.json());
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}`);
+        // route to test GQL API's
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
 });
 
+// With the new ApolloServer() function, we provide the type definitions and resolvers so they know what our API looks like and how it resolves requests. There are more parameters we could pass in, but these are the two we really need to get started.
+
+//We then connect our Apollo server to our Express.js server. This will create a special /graphql endpoint for the Express.js server that will serve as the main endpoint for accessing the entire API. That's not all—the /graphql endpoint also has a built-in testing tool we can use. Let's check it out!
